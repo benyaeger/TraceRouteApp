89e543d4c4d0dba1a182425f939d3633df99274d
benyaeger <benyaeger13@gmail.com>
2024-05-18 11:12:55 +0300

    Version 1.1
    Performance Improvement - Mass packet sending instead of one by one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import socket
 from socket import *
-from scapy.layers.inet import IP, TCP, sr1, ICMP
+from scapy.layers.inet import IP, TCP, sr1, ICMP, sr
 
 
 def tracroute():
@@ -12,19 +12,26 @@ def tracroute():
         hostname = input('Please Enter Hostname To Trace (ex: google.com):')
         try:
             host_address = gethostbyname(hostname)
-            print('Hostname {} was found at {}'.format(hostname, host_address))
+            print('Hostname "{}" was found at {}'.format(hostname, host_address))
             host_valid = True
         except:
             print('Failed to fetch hostname address')
 
-    print('Initiating Traceroute Process...')
+    print('Initiating Traceroute Process')
 
-    max_hops = 15
-    timeout = 8
+    max_hops = 30
+    timeout = 5
 
+    packets = []
     for ttl in range(1, max_hops + 1):
         packet = IP(dst=host_address, ttl=ttl) / TCP(dport=80, flags='S')
-        reply = sr1(packet, timeout=timeout, verbose=0)
+        packets.append(packet)
+
+    answered, unanswered = sr(packets, timeout=timeout, verbose=0)
+    print('Packets Sent: {}, Answered Packets: {}, Unanswered Packets: {}'.format(len(answered) + len(unanswered), len(answered), len(unanswered)))
+    ttl = 0
+    for sent, reply in answered:
+        ttl += 1
         if reply is None:
             print(f'{ttl}\tRequest Timed Out')
         elif reply.haslayer(ICMP):
